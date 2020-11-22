ea84a1b308c1abe0cc793fbcb1d80ab025980a0c
abkhale <abkhale.17@gmail.com>
2020-11-22 18:26:33 +0530

    7.4: anecdotes and hooks step1

--- part7/routed-anecdotes/src/components/Anecdote.js
+++ part7/routed-anecdotes/src/components/Anecdote.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Anecdote = ({ anecdote }) => {
+  if( anecdote === undefined) {
+    return <div style={{color:'red', fontWeight:'bold', margin: '20px'}}>{`{Error: Something Went Wrong, Re-check URL}`}</div>
+  }
   return (
     <div>
       <h2>{anecdote.content}</h2>
