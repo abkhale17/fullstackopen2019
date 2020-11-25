57b23c54cb737d74bd2539ecab21a29c0076177a
abkhale <abkhale.17@gmail.com>
2020-11-25 20:02:15 +0530

    7.7: country hook

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,8 +18,16 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect()
-
+  useEffect(() => {
+    axios.get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+      .then( res => {
+        setCountry({ found: true, data: res.data[0] })
+      })
+      .catch( error => {
+        console.log('error while fetching country')
+        setCountry({ found: false, data: {} })
+    })
+  }, [name])
   return country
 }
 
