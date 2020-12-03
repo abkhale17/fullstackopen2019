082e7b4e0a52d31b5f2930c38e05eac35e212ed4
abkhale <abkhale.17@gmail.com>
2020-12-04 00:32:07 +0530

    8.16 user and logging in

--- part8/library-backend/librayBackend.js
+++ part8/library-backend/librayBackend.js
@@ -1,12 +1,15 @@
 const { ApolloServer, gql, UserInputError, AuthenticationError } = require('apollo-server')
+const jwt = require('jsonwebtoken')
 
 const mongoose = require('mongoose')
 
 const Author = require('./models/author')
 const Book = require('./models/book')
+const User = require('./models/user')
 
 const MONGODB_URI = 'mongodb+srv://fso1920:abhishek@cluster0.rrvky.mongodb.net/Blog_list?retryWrites=true&w=majority'
 
+const JWT_SECRET = 'weatherBOI'
 console.log('connecting to', MONGODB_URI)
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -18,6 +21,16 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,
   })
 
 const typeDefs = gql`
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Book {
     title: String!,
     published: Int!,
@@ -38,6 +51,7 @@ const typeDefs = gql`
     authorCount: Int!,
     allBooks(author: String, genre: String): [Book!]!,
     allAuthors: [Author!],
+    me: User
   }
 
   type Mutation {
@@ -51,6 +65,14 @@ const typeDefs = gql`
       name: String!,
       setBornTo: Int!,
     ) : Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `
 
@@ -84,6 +106,7 @@ const resolvers = {
       })
       return authors
     },
+    me: (root, args, context) => context.currentUser,
   },
   Mutation: {
     addBook: async (root, args) => {
@@ -127,6 +150,30 @@ const resolvers = {
           invalidArgs: args,
         })
       }
+    },
+    createUser: (root, args) => {
+      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+  
+      return user.save()
+        .catch(error => {
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne( { username: args.username })
+      // password kept same for all users for sake of simplicity
+      if( !user || args.password !== 'secret' ) {
+        throw new UserInputError('wrong credentials')
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) }
     }
   }
 }
@@ -134,6 +181,17 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if(auth && auth.toLocaleLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), JWT_SECRET
+      )
+
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
