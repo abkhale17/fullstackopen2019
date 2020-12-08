bb4b7e387fb830fd9bcfd9d41d4c24babd14b13d
abkhale <abkhale.17@gmail.com>
2020-12-08 19:02:54 +0530

    8.25: Subscriptions - client, part 2

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -14,17 +14,14 @@ const App = () => {
   const client = useApolloClient()
 
   const updateCatcheWith = (addedBook) => {
-    const includedIn = (set, object) => set.map(p => p._id).includes(object._id)
-
     const dataInStore = client.readQuery( { query: ALL_BOOKS } )
-    if(!includedIn(dataInStore.allBooks, addedBook)) {
-      client.writeQuery({
-        query: ALL_BOOKS,
-        data: {
-          allBooks : dataInStore.allBooks.concat(addedBook)
-        }
-      })
-    }
+    
+    client.writeQuery({
+      query: ALL_BOOKS,
+      data: {
+        allBooks : dataInStore.allBooks.concat(addedBook)
+      }
+    })
   }
 
   useSubscription(BOOK_ADDED, {
